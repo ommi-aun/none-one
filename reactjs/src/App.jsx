52e81adbf2236cfadc94c2c493efdaee3fb4126b
chaurasia Om <chaurasiaom@ommi.local>
2026-02-24 23:07:20 +0530

    reactjs

---
+++ reactjs/src/App.jsx
@@ -0,0 +1,17 @@
+const App = () => {
+  return (
+    <>
+    <div id = "abc">
+      <h1 id= "h1">fuck you !!!</h1>
+      <h2 id = "h2">fuck you !!!</h2>
+    </div>
+    <div id= "def">
+      <h1 id = "h3">fuck you...</h1>
+    </div>
+    </>
+    
+    
+  );
+}
+
+export default App
